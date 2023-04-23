10f648e25c4e0006840f7c13532f07b863362322
conflabermits <christopher.dunaj@gmail.com>
2023-04-22 21:56:20 -0400

    Added original ChatGPT code

--- gpt-primes.py
+++ gpt-primes.py
@@ -1,2 +1,20 @@
 #!python3
 
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+def generate_primes(n):
+    primes = []
+    for i in range(2, n+1):
+        if is_prime(i):
+            primes.append(i)
+    return primes
+
+n = int(input("Enter the value of n: "))
+print("Prime numbers upto", n, "are:", generate_primes(n))
+
